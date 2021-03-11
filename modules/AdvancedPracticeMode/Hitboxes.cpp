51eaedd2a7731faf4996723a2f7fc0b5fed63be5
Gegel85 <Gegel85@laposte.net>
2021-03-11 23:35:04 +0900

    Added colors for hitboes

--- modules/AdvancedPracticeMode/Hitboxes.cpp
+++ modules/AdvancedPracticeMode/Hitboxes.cpp
@@ -9,6 +9,8 @@
 
 namespace Practice
 {
+#define BOXES_ALPHA 0.5
+
 	static RectangleShape rectangle;
 
 	static void drawBox(const SokuLib::Box &box, const SokuLib::RotationBox *rotation, DxSokuColor borderColor, DxSokuColor fillColor)
@@ -57,7 +59,7 @@ namespace Practice
 		rect.y2 = SokuLib::camera.scale * (box.bottom - std::ceil(manager.position.y) + SokuLib::camera.translate.y);
 
 		rectangle.setRect(rect);
-		rectangle.setFillColor(DxSokuColor::Yellow * 0.5);
+		rectangle.setFillColor(DxSokuColor::Yellow * BOXES_ALPHA);
 		rectangle.setBorderColor(DxSokuColor::Yellow);
 		rectangle.draw();
 	}
@@ -66,16 +68,39 @@ namespace Practice
 	{
 		if (manager.hurtBoxCount > 5)
 			return;
+
+		std::pair<DxSokuColor, DxSokuColor> colors = {DxSokuColor::Green, DxSokuColor::Green * BOXES_ALPHA};
+
+		if (manager.frameData.frameFlags.chOnHit)
+			colors.second = DxSokuColor::Cyan * BOXES_ALPHA;
+		if (manager.frameData.frameFlags.meleeInvincible)
+			colors.second = DxSokuColor::Transparent;
+		if (manager.frameData.frameFlags.grabInvincible)
+			colors.second = {0xA0, 0xFF, 0xA0, 0xFF};
+		if (manager.frameData.frameFlags.graze)
+			colors.second *= 0.5;
+
 		for (int i = 0; i < manager.hurtBoxCount; i++)
-			drawBox(manager.hurtBoxes[i], manager.hurtBoxesRotation[i], DxSokuColor::Green, DxSokuColor::Green * 0.5);
+			drawBox(manager.hurtBoxes[i], manager.hurtBoxesRotation[i], colors.first, colors.second);
 	}
 
 	static void drawHitBoxes(const SokuLib::ObjectManager &manager)
 	{
 		if (manager.hitBoxCount > 5)
 			return;
-		for (int i = 0; i < manager.hitBoxCount && i < 5; i++)
-			drawBox(manager.hitBoxes[i], manager.hitBoxesRotation[i], DxSokuColor::Red, DxSokuColor::Red * 0.5);
+
+		std::pair<DxSokuColor, DxSokuColor> colors = {DxSokuColor::Red, DxSokuColor::Red * BOXES_ALPHA};
+
+		if (manager.frameData.attackFlags.grazable)
+			colors.second = DxSokuColor::Magenta * BOXES_ALPHA;
+		if (!manager.frameData.attackFlags.airBlockable)
+			colors.second = DxSokuColor::Yellow * BOXES_ALPHA;
+
+		if (manager.hitBoxCount)
+			printf("%x\n", manager.frameData.attackFlags.value);
+
+		for (int i = 0; i < manager.hitBoxCount; i++)
+			drawBox(manager.hitBoxes[i], manager.hitBoxesRotation[i], colors.first, colors.second);
 	}
 
 	static void drawPlayerBoxes(const SokuLib::CharacterManager &manager)
