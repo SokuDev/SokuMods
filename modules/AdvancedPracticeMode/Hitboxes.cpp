99ff1a8f342800d05684a783e14d87d696a9228f
Gegel85 <Gegel85@laposte.net>
2021-03-18 13:52:25 +0900

    Added some buffs

--- modules/AdvancedPracticeMode/Hitboxes.cpp
+++ modules/AdvancedPracticeMode/Hitboxes.cpp
@@ -11,20 +11,23 @@
 namespace Practice
 {
 #define BOXES_ALPHA 0.5
-#define GRAZE_SPRITE_OFF       {0,   0}
-#define HJC_SPRITE_OFF         {32,  0}
-#define GRAB_INVUL_SPRITE_OFF  {64,  0}
-#define PROJ_INVUL_SPRITE_OFF  {96,  0}
-#define MELEE_INVUL_SPRITE_OFF {128, 0}
-#define CAN_BLOCK_SPRITE_OFF   {160, 0}
-#define SUPERARMOR_SPRITE_OFF  {192, 0}
-#define GRAZABLE_SPRITE_OFF    {0,   32}
-#define AUB_SPRITE_OFF         {32,  32}
-#define UNBLOCKABLE_SPRITE_OFF {64,  32}
-#define GRAB_SPRITE_OFF        {96,  32}
-#define MILLENIUM_SPRITE_OFF   {128, 32}
-#define DROP_SPRITE_OFF        {160, 32}
-#define HEALTH_SPRITE_OFF      {192, 32}
+#define GRAZE_SPRITE_OFF          {0,   0}
+#define HJC_SPRITE_OFF            {32,  0}
+#define GRAB_INVUL_SPRITE_OFF     {64,  0}
+#define PROJ_INVUL_SPRITE_OFF     {96,  0}
+#define MELEE_INVUL_SPRITE_OFF    {128, 0}
+#define CAN_BLOCK_SPRITE_OFF      {160, 0}
+#define SUPERARMOR_SPRITE_OFF     {192, 0}
+#define GRAZABLE_SPRITE_OFF       {0,   32}
+#define AUB_SPRITE_OFF            {32,  32}
+#define UNBLOCKABLE_SPRITE_OFF    {64,  32}
+#define GRAB_SPRITE_OFF           {96,  32}
+#define MILLENIUM_SPRITE_OFF      {128, 32}
+#define DROP_SPRITE_OFF           {160, 32}
+#define HEALTH_SPRITE_OFF         {192, 32}
+#define FANTASY_HEAVEN_SPRITE_OFF {224, 0}
+#define PHILOSOPHERS_STONE_OFF    {224, 32}
+#define TIME_SPRITE_OFF           {256, 0}
 
 	struct MaxAttributes {
 		unsigned short drop;
@@ -132,9 +135,6 @@ namespace Practice
 		if (manager.hitBoxCount > 5)
 			return;
 
-		if (manager.hitBoxCount)
-			printf("%x\n", manager.frameData.attackFlags.value);
-
 		for (int i = 0; i < manager.hitBoxCount; i++)
 			drawBox(manager.hitBoxes[i], manager.hitBoxesRotation[i], DxSokuColor::Red, DxSokuColor::Red * BOXES_ALPHA);
 	}
@@ -173,6 +173,15 @@ namespace Practice
 			barPos.y += 32;
 		}
 		displayPlayerFrameFlag(textureOffset, barPos, reverse);
+
+		rectangle.setSize({200, 24});
+		rectangle.setBorderColor(DxSokuColor::Black);
+		rectangle.setFillColor({0x88, 0x88, 0x88, 0xFF});
+		if (reverse)
+			rectangle.setPosition({200 - barPos.x, barPos.y + 4});
+		else
+			rectangle.setPosition({barPos.x, barPos.y + 4});
+
 		rectangle.setSize({200 * value / max, 24});
 		rectangle.setBorderColor(DxSokuColor::Black);
 		rectangle.setFillColor(color);
@@ -221,8 +230,8 @@ namespace Practice
 		rectangle.setBorderColor(DxSokuColor::Black);
 		rectangle.setFillColor(color);
 		rectangle.setPosition({
-			static_cast<int>(barPos.x * SokuLib::camera.scale),
-			static_cast<int>((barPos.y + 4) * SokuLib::camera.scale)
+			barPos.x,
+			barPos.y + static_cast<int>(4 * SokuLib::camera.scale)
 		});
 		rectangle.draw();
 	}
@@ -275,7 +284,7 @@ namespace Practice
 		return 0;
 	}
 
-	static void displayPlayerFrameFlags(const SokuLib::CharacterManager &manager, SokuLib::Character character, MaxAttributes &maxAttributes, Vector2<int> barPos, bool reverse)
+	static void displayPlayerStatusFlags(const SokuLib::CharacterManager &manager, SokuLib::Character character, MaxAttributes &maxAttributes, Vector2<int> barPos, bool reverse)
 	{
 		auto basePos = barPos;
 		auto &base = manager.objectBase;
@@ -308,9 +317,26 @@ namespace Practice
 			displayPlayerBar(DROP_SPRITE_OFF, basePos, barPos, manager.dropInvulTimeLeft, maxAttributes.drop, reverse, DxSokuColor::Red);
 		} else
 			maxAttributes.drop = 0;
+
+		switch (character) {
+		case SokuLib::CHARACTER_PATCHOULI:
+			if (manager.philosophersStoneTime)
+				displayPlayerBar(PHILOSOPHERS_STONE_OFF, basePos, barPos, manager.philosophersStoneTime, 1200, reverse, DxSokuColor::Magenta);
+			break;
+		case SokuLib::CHARACTER_REMILIA:
+			if (manager.milleniumVampireTime)
+				displayPlayerBar(MILLENIUM_SPRITE_OFF, basePos, barPos, manager.milleniumVampireTime, 600, reverse, DxSokuColor::Red);
+			break;
+		case SokuLib::CHARACTER_REIMU:
+			if (manager.fantasyHeavenTimeLeft)
+				displayPlayerBar(FANTASY_HEAVEN_SPRITE_OFF, basePos, barPos, manager.fantasyHeavenTimeLeft, 900, reverse, DxSokuColor::Red);
+			break;
+		default:
+			break;
+		}
 	}
 
-	static void displayHitProperties(const SokuLib::ObjectManager &manager, bool showHp = false)
+	static void displayHitProperties(SokuLib::Character baseCharacter, const SokuLib::ObjectManager &manager, bool showHp)
 	{
 		int maxX = 640 - (32 + 50) * SokuLib::camera.scale;
 		int maxY = 480 - 32 * SokuLib::camera.scale;
@@ -398,8 +424,22 @@ namespace Practice
 				displayObjectFrameFlag(GRAZABLE_SPRITE_OFF, base);
 		}
 		if (manager.hurtBoxCount) {
-			if (showHp && manager.hp > 1)
-				displayObjectBar(HEALTH_SPRITE_OFF, leftBound, base, manager.hp, 10000, DxSokuColor::Green);
+			if (showHp) {
+				auto realHp = manager.hp;
+				auto maxHp = 10000;
+
+				if ( //This is Patchouli's bubble
+					baseCharacter == SokuLib::CHARACTER_PATCHOULI &&
+					manager.action == 0x358 &&
+					(manager.image.number == 0x154 || manager.image.number == 0x1B3)
+				) {
+					displayObjectBar(TIME_SPRITE_OFF, leftBound, base, 360 - manager.frameCount, 360, DxSokuColor::Green);
+					realHp += 3000;
+					maxHp = 3000;
+				}
+				if (realHp > 1)
+					displayObjectBar(HEALTH_SPRITE_OFF, leftBound, base, realHp, maxHp, DxSokuColor::Green);
+			}
 		}
 	}
 
@@ -409,8 +449,8 @@ namespace Practice
 		drawHurtBoxes(manager.objectBase);
 		drawHitBoxes(manager.objectBase);
 		drawPositionBox(manager.objectBase);
-		displayPlayerFrameFlags(manager, character, maxAttributes, barPos, reverse);
-		displayHitProperties(manager.objectBase);
+		displayPlayerStatusFlags(manager, character, maxAttributes, barPos, reverse);
+		displayHitProperties(character, manager.objectBase, false);
 
 		auto array = manager.objects.list.vector();
 
@@ -423,7 +463,7 @@ namespace Practice
 				drawHitBoxes(elem->objectBase);
 				drawPositionBox(elem->objectBase);
 				displayObjectFrameFlags(elem->objectBase);
-				displayHitProperties(elem->objectBase, true);
+				displayHitProperties(character, elem->objectBase, true);
 			}
 		}
 	}
