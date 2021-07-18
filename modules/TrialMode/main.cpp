2b15f752e797fb8cad7e8b49b554effd299936a0
Gegel85 <Gegel85@laposte.net>
2021-07-18 21:52:29 +0200

    Added basic mod

---
+++ modules/TrialMode/main.cpp
@@ -0,0 +1,188 @@
+//
+// Created by Gegel85 on 31/10/2020
+//
+
+#include "nlohmann/json.hpp"
+#include <SokuLib.hpp>
+#include <fstream>
+#include <shlwapi.h>
+#include <string>
+#include <thread>
+#include <sstream>
+
+#ifndef _DEBUG
+#define puts(...)
+#define printf(...)
+#endif
+
+static std::vector<SokuLib::Character> validCharacters{
+	SokuLib::CHARACTER_REIMU,
+	SokuLib::CHARACTER_MARISA,
+	SokuLib::CHARACTER_SAKUYA,
+	SokuLib::CHARACTER_ALICE,
+	SokuLib::CHARACTER_PATCHOULI,
+	SokuLib::CHARACTER_YOUMU,
+	SokuLib::CHARACTER_REMILIA,
+	SokuLib::CHARACTER_YUYUKO,
+	SokuLib::CHARACTER_YUKARI,
+	SokuLib::CHARACTER_SUIKA,
+	SokuLib::CHARACTER_REISEN,
+	SokuLib::CHARACTER_AYA,
+	SokuLib::CHARACTER_KOMACHI,
+	SokuLib::CHARACTER_IKU,
+	SokuLib::CHARACTER_TENSHI,
+	SokuLib::CHARACTER_SANAE,
+	SokuLib::CHARACTER_CIRNO,
+	SokuLib::CHARACTER_MEILING,
+	SokuLib::CHARACTER_UTSUHO,
+	SokuLib::CHARACTER_SUWAKO
+};
+
+void loadSoku2CSV(LPWSTR path)
+{
+	std::ifstream stream{path};
+	std::string line;
+
+	if (stream.fail()) {
+		printf("%S: %s\n", path, strerror(errno));
+		return;
+	}
+	while (std::getline(stream, line)) {
+		std::stringstream str{line};
+		unsigned id;
+		std::string idStr;
+
+		std::getline(str, idStr, ';');
+		if (str.fail()) {
+			printf("Skipping line %s: Stream failed\n", line.c_str());
+			continue;
+		}
+		try {
+			id = std::stoi(idStr);
+		} catch (...){
+			printf("Skipping line %s: Invalid id\n", line.c_str());
+			continue;
+		}
+		validCharacters.push_back(static_cast<SokuLib::Character>(id));
+	}
+}
+
+void loadSoku2Config()
+{
+	puts("Looking for Soku2 config...");
+
+	int argc;
+	wchar_t app_path[MAX_PATH];
+	wchar_t setting_path[MAX_PATH];
+	wchar_t **arg_list = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+	wcsncpy(app_path, arg_list[0], MAX_PATH);
+	PathRemoveFileSpecW(app_path);
+	if (GetEnvironmentVariableW(L"SWRSTOYS", setting_path, sizeof(setting_path)) <= 0) {
+		if (arg_list && argc > 1 && StrStrIW(arg_list[1], L"ini")) {
+			wcscpy(setting_path, arg_list[1]);
+			LocalFree(arg_list);
+		} else {
+			wcscpy(setting_path, app_path);
+			PathAppendW(setting_path, L"\\SWRSToys.ini");
+		}
+		if (arg_list) {
+			LocalFree(arg_list);
+		}
+	}
+	printf("Config file is %S\n", setting_path);
+
+	wchar_t moduleKeys[1024];
+	wchar_t moduleValue[MAX_PATH];
+	GetPrivateProfileStringW(L"Module", nullptr, nullptr, moduleKeys, sizeof(moduleKeys), setting_path);
+	for (wchar_t *key = moduleKeys; *key; key += wcslen(key) + 1) {
+		wchar_t module_path[MAX_PATH];
+
+		GetPrivateProfileStringW(L"Module", key, nullptr, moduleValue, sizeof(moduleValue), setting_path);
+
+		wchar_t *filename = wcsrchr(moduleValue, '/');
+
+		printf("Check %S\n", moduleValue);
+		if (!filename)
+			filename = app_path;
+		else
+			filename++;
+		for (int i = 0; filename[i]; i++)
+			filename[i] = tolower(filename[i]);
+		if (wcscmp(filename, L"soku2.dll") != 0)
+			continue;
+
+		wcscpy(module_path, app_path);
+		PathAppendW(module_path, moduleValue);
+		while (auto result = wcschr(module_path, '/'))
+			*result = '\\';
+		PathRemoveFileSpecW(module_path);
+		printf("Found Soku2 module folder at %S\n", module_path);
+		PathAppendW(module_path, L"\\config\\info\\characters.csv");
+		loadSoku2CSV(module_path);
+		return;
+	}
+}
+
+void loadPack(char *path)
+{
+
+}
+
+void loadAllPacks(LPCSTR profilePath, LPCSTR profileFolderPath, char *path)
+{
+
+}
+
+// �ݒ胍�[�h
+void LoadSettings(LPCSTR profilePath, LPCSTR profileFolderPath)
+{
+	char buffer[MAX_PATH];
+
+	puts("Loading settings...");
+	GetPrivateProfileStringA("TrialMode", "RefreshTime", "packs", buffer, sizeof(buffer), profilePath);
+	loadAllPacks(profilePath, profileFolderPath, buffer);
+}
+
+extern "C" __declspec(dllexport) bool CheckVersion(const BYTE hash[16]) {
+	return memcmp(hash, SokuLib::targetHash, 16) == 0;
+}
+
+extern "C" __declspec(dllexport) bool Initialize(HMODULE hMyModule, HMODULE hParentModule) {
+	char profilePath[1024 + MAX_PATH];
+	char profileFolderPath[1024 + MAX_PATH];
+
+#ifdef _DEBUG
+	FILE *_;
+
+	AllocConsole();
+	freopen_s(&_, "CONOUT$", "w", stdout);
+	freopen_s(&_, "CONOUT$", "w", stderr);
+#endif
+
+	puts("Initializing...");
+
+	GetModuleFileName(hMyModule, profilePath, 1024);
+	PathRemoveFileSpec(profilePath);
+	strcpy(profileFolderPath, profilePath);
+	PathAppend(profilePath, "DiscordIntegration.ini");
+	LoadSettings(profilePath, profileFolderPath);
+
+	// DWORD old;
+	//::VirtualProtect((PVOID)rdata_Offset, rdata_Size, PAGE_EXECUTE_WRITECOPY, &old);
+	// s_origCLogo_OnProcess   = TamperDword(vtbl_CLogo   + 4, (DWORD)CLogo_OnProcess);
+	// s_origCBattle_OnProcess = TamperDword(vtbl_CBattle + 4, (DWORD)CBattle_OnProcess);
+	// s_origCBattleSV_OnProcess = TamperDword(vtbl_CBattleSV + 4, (DWORD)CBattleSV_OnProcess);
+	// s_origCBattleCL_OnProcess = TamperDword(vtbl_CBattleCL + 4, (DWORD)CBattleCL_OnProcess);
+	// s_origCTitle_OnProcess  = TamperDword(vtbl_CTitle  + 4, (DWORD)CTitle_OnProcess);
+	// s_origCSelect_OnProcess = TamperDword(vtbl_CSelect + 4, (DWORD)CSelect_OnProcess);
+	//::VirtualProtect((PVOID)rdata_Offset, rdata_Size, old, &old);
+
+	//::FlushInstructionCache(GetCurrentProcess(), nullptr, 0);
+	puts("Done...");
+	return true;
+}
+
+extern "C" int APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID lpReserved) {
+	return TRUE;
+}
