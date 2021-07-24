7b1158cbae39965d8ed85b218b6d9a1c41f5add8
Benoît Pineau <benoitpineau02@gmail.com>
2021-07-24 17:39:34 +0200

    every texture useful are be loaded

--- modules/TrialMode/BattleAnimation.cpp
+++ modules/TrialMode/BattleAnimation.cpp
@@ -1,17 +1,14 @@
 //
-// Created by 2deg on 20/07/2021.
+// Created by 2deg and PinkySmile on 20/07/2021.
 //
 
 #include <memory>
+#include <utility>
 #include "BattleAnimation.hpp"
 #include "Menu.hpp"
+#include "Pack.hpp"
 
-//Beware to modify Text size too much (because antialiasing)
-/*bool loadBattleAnimationTextOnTextBox(char *dialogue, SokuLib::Vector2<unsigned int> textBoxSize)
-{
-
-}
-*/
+//MovingSprite Function (manage loading and rendering all sprite individually)
 
 MovingSprite::MovingSprite(const char *path, bool *success)
 {
@@ -21,17 +18,74 @@ MovingSprite::MovingSprite(const char *path, bool *success)
 		*success = result;
 }
 
-MovingSprite::MovingSprite(const char *text, SokuLib::SWRFont &font)
+MovingSprite::MovingSprite(const char *text, SokuLib::SWRFont &font, bool *success)
 {
 	SokuLib::Vector2i buffer;
 
-	this->sprite.texture.createFromText(text, defaultFont16, {1300, 60}, &buffer);
+	bool result = this->sprite.texture.createFromText(text, font, {1300, 60}, &buffer);
 	this->sprite.setSize(buffer.to<unsigned>());
 	this->sprite.rect.width = buffer.x;
 	this->sprite.rect.height = buffer.y;
+
+	if (success)
+		*success = result;
 }
 
-void update(SokuLib::Vector2f velocity = {0, 0}, SokuLib::Vector2f angularVelocity = {0, 0});
+
+
+const SokuLib::DrawUtils::Sprite &MovingSprite::render(CharacterSide characterFollowed,
+						       SokuLib::Vector2u spriteSize,
+						       SokuLib::Vector2f positionOffset,
+						       const SokuLib::Camera *camera,
+						       SokuLib::Vector2u textureSize,
+						       SokuLib::Vector2b mirror,
+						       float rotation) const
+{
+	this->sprite.setCamera(camera);
+
+	this->sprite.setMirroring(mirror.x, mirror.y);
+	if (characterFollowed == NONE){
+		this->sprite.setPosition(positionOffset.to<int>());
+	} else {
+		if (characterFollowed == LEFTCHAR) {
+			this->sprite.setPosition(SokuLib::Vector2i{
+				static_cast<int>(SokuLib::getBattleMgr().leftCharacterManager.objectBase.position.x +
+						 positionOffset.x + this->j.x),
+				static_cast<int>(-SokuLib::getBattleMgr().leftCharacterManager.objectBase.position.y +
+						 positionOffset.y + this->j.y),
+			});
+		} else {
+			this->sprite.setPosition(SokuLib::Vector2i{
+				static_cast<int>(SokuLib::getBattleMgr().rightCharacterManager.objectBase.position.x +
+						 positionOffset.x),
+				static_cast<int>(-SokuLib::getBattleMgr().rightCharacterManager.objectBase.position.y +
+						 positionOffset.y),
+			});
+		}
+	}
+	if (textureSize == SokuLib::Vector2i {0, 0}){
+		this->sprite.setSize(this->sprite.texture.getSize());
+	} else {
+		this->sprite.setSize(textureSize);
+	}
+
+	sprite.rect.top = sprite.rect.width = 0;
+	sprite.rect.width = sprite.texture.getSize().x;
+	sprite.rect.height = sprite.texture.getSize().y;
+	this->sprite.setRotation(rotation + this->i);
+	this->sprite.draw();
+
+	return sprite;
+}
+
+void MovingSprite::update(SokuLib::Vector2f velocity, float angularVelocity)
+{
+	this->i += angularVelocity;
+	this->j += velocity;
+	return;
+}
+
+//Function to load multiple texture at once
 
 bool loadBattleInitSpriteOnce(const char *path, std::vector<std::unique_ptr<MovingSprite>> &spriteVec)
 {
@@ -41,82 +95,87 @@ bool loadBattleInitSpriteOnce(const char *path, std::vector<std::unique_ptr<Movi
 	return result;
 }
 
-SokuLib::DrawUtils::Sprite &renderBattleAnimationSprite(MovingSprite &sprite,
-							CharacterSide characterFollowed,
-							SokuLib::Vector2u textureSize,
-							SokuLib::Vector2f positionOffset,
-							const SokuLib::Camera *camera,
-							SokuLib::Vector2b Mirror,
-							SokuLib::Vector2f movement,
-							float rotation)
+bool loadBattleInitTextOnce(const char *text, std::vector<std::unique_ptr<MovingSprite>> &spriteVec)
 {
-	sprite.sprite.setCamera(camera);
+	bool result;
 
-	sprite.sprite.setMirroring(Mirror.x, Mirror.y);
-	if (characterFollowed == NONE){
-		sprite.sprite.setPosition(positionOffset.to<int>() + sprite.j.to<int>());
-	} else {
-		if (characterFollowed == LEFTCHAR) {
-			sprite.sprite.setPosition(SokuLib::Vector2i{
-				static_cast<int>(SokuLib::getBattleMgr().leftCharacterManager.objectBase.position.x +
-				positionOffset.x + sprite.j.x),
-				static_cast<int>(-SokuLib::getBattleMgr().leftCharacterManager.objectBase.position.y +
-				positionOffset.y + sprite.j.y),
-			});
-		} else {
-			sprite.sprite.setPosition(SokuLib::Vector2i{
-				static_cast<int>(SokuLib::getBattleMgr().rightCharacterManager.objectBase.position.x +
-						 positionOffset.x + sprite.j.x),
-				static_cast<int>(-SokuLib::getBattleMgr().rightCharacterManager.objectBase.position.y +
-						 positionOffset.y + sprite.j.y),
-			});
-
-		}
-	}
-	if (textureSize == SokuLib::Vector2i {0, 0}){
-		sprite.sprite.setSize(sprite.sprite.texture.getSize());
-	} else{
-		sprite.sprite.setSize(textureSize);
-	}
-
-	sprite.sprite.rect.top = sprite.sprite.rect.width = 0;
-	sprite.sprite.rect.width = sprite.sprite.texture.getSize().x;
-	sprite.sprite.rect.height = sprite.sprite.texture.getSize().y;
-	sprite.sprite.setRotation(sprite.i);
-	sprite.sprite.draw();
-
-	sprite.i = sprite.i + rotation;
-	sprite.j = sprite.j + movement;
-
-	return sprite.sprite;
+	spriteVec.emplace_back(new MovingSprite{text,defaultFont16, &result});
+	return result;
 }
 
-Dialog::Dialog(const char *dialogLoad, DBoxType dBoxType)
-{
-	SokuLib::Vector2i buffer;
+//SokuDialog function (responsible of making dialog box including the text within)
 
-	if (dBoxType == TALKINGBOX) {
-		initBattleAnimation(this->_spriteVec,
+SokuDialog::SokuDialog(std::vector<std::string> dialogLoad) {
+	initBattleAnimation(this->_spriteVec, loadBattleInitSpriteOnce,
 				    "data/scenario/effect/windowRight.bmp",
 				    "data/scenario/effect/windowLoop.bmp",
 				    "data/scenario/effect/windowLeft.bmp",
 				    "data/scenario/effect/windowCenter.bmp");
-	} else {
-		initBattleAnimation(this->_spriteVec,
-				    "data/scenario/effect/windowRight.bmp",
-				    "data/scenario/effect/windowLoop.bmp",
-				    "data/scenario/effect/windowLeft.bmp",
-				    "data/scenario/effect/windowCenterB.bmp");
-	}
-	this->_spriteVec.emplace_back(new MovingSprite);
-	this->_spriteVec.back()->sprite.texture.createFromText(dialogLoad, defaultFont16, {1300, 60}, &buffer);
-	this->_spriteVec.back()->sprite.setSize(buffer.to<unsigned>());
-	this->_spriteVec.back()->sprite.rect.width = buffer.x;
-	this->_spriteVec.back()->sprite.rect.height = buffer.y;
+	initBattleAnimation(this->_spriteVec, loadBattleInitTextOnce, std::move(dialogLoad));
 }
 
-void Dialog::renderDialogWithBox(const SokuLib::CharacterManager &charSide)
-{
-	renderBattleAnimationSprite(*this->_spriteVec[0], NONE, {0,0,}, {charSide.objectBase.position.x - 100, charSide.objectBase.position.y - 250}, &SokuLib::camera);
+//SokuThinking function (load the thinking type box)
 
+SokuThinking::SokuThinking(std::vector<std::string> dialogLoad) :
+	SokuDialog(std::move(dialogLoad))
+{
+
+	initBattleAnimation(this->_spriteVec, loadBattleInitSpriteOnce,
+			    "data/scenario/effect/windowCenterB.bmp");
+}
+
+
+//SokuTalking function (load the talking type box)
+
+SokuTalking::SokuTalking(std::vector<std::string> dialogLoad) :
+	SokuDialog(std::move(dialogLoad))
+{
+
+	initBattleAnimation(this->_spriteVec, loadBattleInitSpriteOnce,
+			    "data/scenario/effect/windowCenter.bmp");
+}
+
+//SokuStand function (responsible of making result box including text and stand within)
+
+const char *getChrCodeName(SokuLib::Character chr)
+{
+	try {
+		return validCharacters.at(chr).c_str();
+	} catch (...) {}
+	if (SokuLib::SWRUnlinked)
+		return nullptr;
+	try {
+		return swrCharacters.at(chr).c_str();
+	} catch (...) {}
+	return nullptr;
+}
+
+SokuStand::SokuStand(std::vector<std::string> dialogLoad)
+{
+	std::string leftChar = getChrCodeName(SokuLib::leftChar);
+	std::string rightChar = getChrCodeName(SokuLib::rightChar);
+	this->StandDBox.setSize({624, 80});
+	this->StandDBox.setPosition({8, 392});
+
+	initBattleAnimation(this->_spriteVec, loadBattleInitSpriteOnce,
+			    ("data/character/" + leftChar + "/stand/è-.bmp").c_str(),
+			    ("data/character/" + leftChar + "/stand/è¥.bmp").c_str(),
+			    ("data/character/" + leftChar + "/stand/ï-.bmp").c_str(),
+			    ("data/character/" + leftChar + "/stand/îê.bmp").c_str(),
+			    ("data/character/" + leftChar + "/stand/ô{.bmp").c_str(),
+			    ("data/character/" + leftChar + "/stand/òë.bmp").c_str(),
+			    ("data/character/" + leftChar + "/stand/òë.bmp").c_str(),
+			    ("data/character/" + leftChar + "/stand/ù].bmp").c_str(),
+			    ("data/character/" + leftChar + "/stand/ÿf.bmp").c_str());
+	initBattleAnimation(this->_spriteVec, loadBattleInitSpriteOnce,
+			    ("data/character/" + rightChar + "/stand/è-.bmp").c_str(),
+			    ("data/character/" + rightChar + "/stand/è¥.bmp").c_str(),
+			    ("data/character/" + rightChar + "/stand/ï-.bmp").c_str(),
+			    ("data/character/" + rightChar + "/stand/îê.bmp").c_str(),
+			    ("data/character/" + rightChar + "/stand/ô{.bmp").c_str(),
+			    ("data/character/" + rightChar + "/stand/òë.bmp").c_str(),
+			    ("data/character/" + rightChar + "/stand/òë.bmp").c_str(),
+			    ("data/character/" + rightChar + "/stand/ù].bmp").c_str(),
+			    ("data/character/" + rightChar + "/stand/ÿf.bmp").c_str());
+	initBattleAnimation(this->_spriteVec, loadBattleInitTextOnce, dialogLoad);
 }
