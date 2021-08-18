efb71780a5780a83ca28af549e9f209c9af894ce
Gegel85 <Gegel85@laposte.net>
2021-08-18 17:28:11 +0200

    Ported all C++ animations to lua

---
+++ modules/TrialMode/packs/remilia_story/bnbmid2outro.lua
@@ -0,0 +1,82 @@
+--
+-- Created by PinkySmile on 18/08/2021.
+--
+
+local dialogs = {
+	"lHDNow, you know how to deal with uninvited guests.",
+	"rHDI'll be sure to remember...",
+	"lhDOh, by the way could you give that to Patchouli?",
+	"rhDA feather? I will give it to her if I see her.",
+	"lhDGood, I'm sure she'll find some use to it."
+	--Battle here
+}
+
+local dialog  = StandDialog.new(dialogs)
+local pressed = false
+local side = false
+local ctr = 60
+local flashRect = RectangleShape.new()
+
+dialog.hidden = false
+flashRect.borderColor = enums.colors.Transparent
+flashRect.fillColor = enums.colors.Transparent
+flashRect.size = Vector2u.new(640, 480)
+
+function update()
+	local color = flashRect.fillColor
+
+	battleMgr.leftCharacterManager:updateAnimation()
+	battleMgr.rightCharacterManager:updateAnimation()
+	if color.a and ctr == 0 and side then
+		dialog:update()
+		if pressed then
+			if not dialog:onKeyPress() then
+				return false
+			end
+			pressed = false
+		end
+	end
+	if not side then
+		color.a = color.a + 0x11;
+		if color.a == 0xFF then
+			side = true;
+			camera.translate.x = -320
+			camera.translate.y = 420
+			camera.backgroundTranslate.x = 640
+			camera.backgroundTranslate.y = 0
+
+			battleMgr.leftCharacterManager.position.x = 450
+			battleMgr.leftCharacterManager.position.y = 0
+			battleMgr.leftCharacterManager.actionBlockId = 0
+			battleMgr.leftCharacterManager.animationCounter = 0
+			battleMgr.leftCharacterManager.animationSubFrame = 0
+			battleMgr.leftCharacterManager.action = enums.actions.ACTION_IDLE
+			battleMgr.leftCharacterManager:initAnimation()
+
+			battleMgr.rightCharacterManager.position.x = 800
+			battleMgr.rightCharacterManager.position.y = 0
+			battleMgr.rightCharacterManager.animationSubFrame = 0
+			battleMgr.rightCharacterManager.action = enums.actions.ACTION_IDLE
+			battleMgr.rightCharacterManager:initAnimation()
+		end
+	elseif flashRect.fillColor.a ~= 0 then
+		color.a = color.a - 0x11;
+	elseif ctr ~= 0 then
+		ctr = ctr - 1
+	end
+	flashRect.fillColor = color
+	return true;
+end
+
+function render()
+	flashRect:draw()
+	if flashRect.fillColor.a == 0 and ctr == 0 and side then
+		dialog:render()
+	end
+end
+
+function onKeyPressed()
+	if flashRect.fillColor.a and ctr == 0 and side then
+		pressed = true
+	end
+end
