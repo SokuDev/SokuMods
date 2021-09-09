007354b4e1c195313d657a65ee1802bc21be9d61
Gegel85 <Gegel85@laposte.net>
2021-09-09 10:58:43 +0200

    Fix ambiguous expression

--- modules/TrialMode/ComboTrial.cpp
+++ modules/TrialMode/ComboTrial.cpp
@@ -72,7 +72,7 @@ ComboTrial::ComboTrial(const char *folder, SokuLib::Character player, const nloh
 	if (!json["dummy"]["pos"].contains("y") || !json["dummy"]["pos"]["y"].is_number())
 		throw std::invalid_argument(R"(The field "y" of the field "pos" in the "dummy" field is not present or invalid.)");
 
-	this->_failTimer = json.contains("fail_timer") && json["fail_timer"].is_number() ? json["fail_timer"] : 60;
+	this->_failTimer = json.contains("fail_timer") && json["fail_timer"].is_number() ? json["fail_timer"].get<int>() : 60;
 	this->_crouching = json["dummy"].contains("crouch") && json["dummy"]["crouch"].is_boolean() && json["dummy"]["crouch"].get<bool>();
 	this->_leftWeather = !json["player"].contains("affected_by_weather") || !json["player"]["affected_by_weather"].is_boolean() || json["player"]["affected_by_weather"].get<bool>();
 	this->_rightWeather = !json["dummy"].contains("affected_by_weather") || !json["dummy"]["affected_by_weather"].is_boolean() || json["dummy"]["affected_by_weather"].get<bool>();
