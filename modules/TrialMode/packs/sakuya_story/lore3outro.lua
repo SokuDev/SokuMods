c39ab4bc669715a2e0d8cfb1237aabf2f8e65659
Gegel85 <Gegel85@laposte.net>
2021-10-19 02:26:59 +0200

    Added Sakuya intro outro trial 3

---
+++ modules/TrialMode/packs/sakuya_story/lore3outro.lua
@@ -0,0 +1,80 @@
+--
+-- Created by PinkySmile on 18/08/2021.
+--
+
+local dialogs = {
+	"r DHow could you?",
+	"lEDI'm truly sorry...<br>I just have her best interest in mind.",
+	"rEDFine... I won't tell her anything about it.",
+	"rEEBut could you please help me stand up?",
+	"lWEOf course!"
+}
+
+local dialog  = StandDialog.new(dialogs)
+local pressed = false
+local side = false
+local ctr = 60
+local flashRect = RectangleShape.new()
+
+dialog.hidden = false
+flashRect.borderColor = enums.colors.Transparent
+flashRect.fillColor = enums.colors.Transparent
+flashRect.size = Vector2u.new(640, 480)
+
+function update()
+	local color = flashRect.fillColor
+
+	battleMgr.leftCharacterManager:updateAnimation()
+	if color.a and ctr == 0 and side then
+		dialog:update()
+		if pressed then
+			if not dialog:onKeyPress() then
+				return false
+			end
+			pressed = false
+		end
+	end
+	if not side then
+		color.a = color.a + 0x11;
+		if color.a == 0xFF then
+			side = true;
+			camera.translate.x = -500
+			camera.translate.y = 420
+			camera.backgroundTranslate.x = 900
+			camera.backgroundTranslate.y = 0
+
+			battleMgr.leftCharacterManager.position.x = 800
+			battleMgr.leftCharacterManager.position.y = 0
+			battleMgr.leftCharacterManager.actionBlockId = 0
+			battleMgr.leftCharacterManager.animationCounter = 0
+			battleMgr.leftCharacterManager.animationSubFrame = 0
+			battleMgr.leftCharacterManager.action = enums.actions.ACTION_IDLE
+			battleMgr.leftCharacterManager:initAnimation()
+
+			battleMgr.rightCharacterManager.position.x = 1000
+			battleMgr.rightCharacterManager.position.y = 0
+			battleMgr.rightCharacterManager.animationSubFrame = 0
+			battleMgr.rightCharacterManager.action = enums.actions.ACTION_KNOCKED_DOWN_STATIC
+			battleMgr.rightCharacterManager:initAnimation()
+		end
+	elseif flashRect.fillColor.a ~= 0 then
+		color.a = color.a - 0x11;
+	elseif ctr ~= 0 then
+		ctr = ctr - 1
+	end
+	flashRect.fillColor = color
+	return true;
+end
+
+function render()
+	flashRect:draw()
+	if flashRect.fillColor.a == 0 and ctr == 0 and side then
+		dialog:render()
+	end
+end
+
+function onKeyPressed()
+	if flashRect.fillColor.a and ctr == 0 and side then
+		pressed = true
+	end
+end
