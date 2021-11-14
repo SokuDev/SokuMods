1537230b665a0648bef6e76745e1eca7dd3010ee
PC_volt <PC_volt@DESKTOP-I20R60L>
2021-11-14 13:32:47 +0100

    punish display mod

--- modules/PunishDisplay/main.cpp
+++ modules/PunishDisplay/main.cpp
@@ -1,76 +1,132 @@
 //
 // Created by PinkySmile on 01/08/2021.
+// Edited by PC_volt
 //
 
-#include <fstream>
-#include <dinput.h>
 #include <SokuLib.hpp>
-
-static SokuLib::SWRFont font;
-static SokuLib::DrawUtils::Sprite punishText;
-static std::pair<unsigned, unsigned> dispCtr = {1000, 1000};
+#include <dinput.h>
+#include <fstream>
+static void (SokuLib::BattleManager::*og_BattleManagerOnRoundstart)();
 static void (SokuLib::BattleManager::*og_BattleManagerOnRender)();
 static int (SokuLib::BattleManager::*og_BattleManagerOnProcess)();
-
+static int (SokuLib::CharacterManager::*original_onHit)(int param);
 #ifndef _DEBUG
 #define puts(...)
 #define printf(...)
+
 #endif
 
-static int (SokuLib::CharacterManager::*original_onHit)(int param);
+static SokuLib::DrawUtils::Sprite bePunish;
+static SokuLib::DrawUtils::Sprite jumpPunish;
+static SokuLib::DrawUtils::Sprite dashPunish;
+static SokuLib::DrawUtils::Sprite attackPunish;
+static SokuLib::DrawUtils::Sprite punish;
+
+static SokuLib::SWRFont font;
+static SokuLib::DrawUtils::Sprite* punishTextP1;
+static SokuLib::DrawUtils::Sprite* punishTextP2;
+static std::pair<unsigned, unsigned> displayCounter = {1000, 1000};
+
+SokuLib::DrawUtils::Sprite* associatePunishSprite(SokuLib::CharacterManager &character)
+{
+	char* punishMessage;
+	if ((character.objectBase.action >= SokuLib::ACTION_NEUTRAL_HIGH_JUMP && character.objectBase.action <= SokuLib::ACTION_FORWARD_HIGH_JUMP_FROM_GROUND_DASH)
+			|| (character.objectBase.action >= SokuLib::ACTION_NEUTRAL_JUMP && character.objectBase.action <= SokuLib::ACTION_BACKWARD_JUMP))
+	{
+		return &jumpPunish;
+	}
+	else if (character.objectBase.action >= SokuLib::ACTION_FORWARD_DASH && character.objectBase.action <= SokuLib::ACTION_LILIPAD_BACKDASH)
+	{
+		return &dashPunish;
+	}
+	else if (character.objectBase.action >= SokuLib::ACTION_BE2 && character.objectBase.action <= SokuLib::ACTION_jBE6)
+	{
+		return &bePunish;
+	}
+	else if (character.objectBase.action >= SokuLib::ACTION_5A)
+	{
+		return &attackPunish;
+	}
+	else
+	{
+		return &punish;
+	}
+}
 
 int __fastcall isHit(SokuLib::CharacterManager &character, int, int param)
 {
-	if (
-		!character.objectBase.frameData.frameFlags.guardAvailable &&
+	if (!character.objectBase.frameData.frameFlags.guardAvailable &&
 		(character.objectBase.action < SokuLib::ACTION_STAND_GROUND_HIT_SMALL_HITSTUN ||
-		character.objectBase.action > SokuLib::ACTION_NEUTRAL_TECH)
-	) {
+		character.objectBase.action > SokuLib::ACTION_NEUTRAL_TECH))
+	{
 		if (&character == &SokuLib::getBattleMgr().leftCharacterManager)
-			dispCtr.second = 0;
+		{
+			punishTextP1 = associatePunishSprite(character);
+			displayCounter.first = 0;
+		}
 		else if (&character == &SokuLib::getBattleMgr().rightCharacterManager)
-			dispCtr.first = 0;
+		{
+			punishTextP2 = associatePunishSprite(character);
+			displayCounter.second = 0;
+		}
 	}
 
 	return (character.*original_onHit)(param);
+}
+
+void createPunishTextSprite(SokuLib::DrawUtils::Sprite &punishText, char* punishMessage)
+{
+	SokuLib::FontDescription desc;
+
+	desc.r1 = 255;
+	desc.r2 = 255;
+	desc.g1 = 255;
+	desc.g2 = 255;
+	desc.b1 = 255;
+	desc.b2 = 255;
+	desc.height = 24;
+	desc.weight = FW_BOLD;
+	desc.italic = 0;
+	desc.shadow = 4;
+	desc.bufferSize = 1000000;
+	desc.charSpaceX = 0;
+	desc.charSpaceY = 0;
+	desc.offsetX = 0;
+	desc.offsetY = 0;
+	desc.useOffset = 0;
+	strcpy(desc.faceName, "MonoSpatialModSWR");
+	font.create();
+	font.setIndirect(desc);
+
+	SokuLib::Vector2<int> realSize;
+	if (!punishText.texture.createFromText(punishMessage, font, {0x1000, 50}, &realSize))
+	{
+		puts("Create from text failed");
+	}
+	punishText.setSize(realSize.to<unsigned>());
+	punishText.rect.width = realSize.x;
+	punishText.rect.height = realSize.y;
+}
+
+void createSprites()
+{
+	createPunishTextSprite(jumpPunish, "Jump Punish!");
+	createPunishTextSprite(bePunish, "BE Punish!");
+	createPunishTextSprite(dashPunish, "Dash Punish!");
+	createPunishTextSprite(attackPunish, "Attack Punish!");
+	createPunishTextSprite(punish, "Punish!");
+}
+
+void __fastcall BattleOnRoundstart(SokuLib::BattleManager *This)
+{
+	(This->*og_BattleManagerOnRoundstart)();
+	createSprites();
 }
 
 int __fastcall BattleOnProcess(SokuLib::BattleManager *This)
 {
 	int ret = (This->*og_BattleManagerOnProcess)();
 
-	if (!punishText.texture.hasTexture()) {
-		SokuLib::FontDescription desc;
-
-		desc.r1 = 255;
-		desc.r2 = 255;
-		desc.g1 = 255;
-		desc.g2 = 255;
-		desc.b1 = 255;
-		desc.b2 = 255;
-		desc.height = 24;
-		desc.weight = FW_BOLD;
-		desc.italic = 0;
-		desc.shadow = 4;
-		desc.bufferSize = 1000000;
-		desc.charSpaceX = 0;
-		desc.charSpaceY = 0;
-		desc.offsetX = 0;
-		desc.offsetY = 0;
-		desc.useOffset = 0;
-		strcpy(desc.faceName, "MonoSpatialModSWR");
-		font.create();
-		font.setIndirect(desc);
-
-		punishText.texture.createFromText("Punish!", font, {0x1000, 50});
-		punishText.setSize(punishText.texture.getSize());
-		punishText.rect.width = punishText.texture.getSize().x;
-		punishText.rect.height = punishText.texture.getSize().y;
-	}
-	if (dispCtr.first < 1000)
-		dispCtr.first++;
-	if (dispCtr.second < 1000)
-		dispCtr.second++;
 	return ret;
 }
 
@@ -78,17 +134,25 @@ void __fastcall BattleOnRender(SokuLib::BattleManager *This)
 {
 	(This->*og_BattleManagerOnRender)();
 
-	punishText.setPosition({17, 67});
-	punishText.tint.a = min(255, max(0, (240 - static_cast<int>(dispCtr.first * 4) + 255)));
-	punishText.draw();
-	punishText.setPosition({535, 67});
-	punishText.tint.a = min(255, max(0, (240 - static_cast<int>(dispCtr.second * 4) + 255)));
-	punishText.draw();
-}
+	if (displayCounter.first < 1000)
+		displayCounter.first++;
+	if (displayCounter.second < 1000)
+		displayCounter.second++;
 
-// �ݒ胍�[�h
-void LoadSettings()
-{
+	if (displayCounter.first < 1000)
+	{
+		punishTextP1->setPosition({17, 67});
+		punishTextP1->tint.a = min(255, max(0, (240 - static_cast<int>(displayCounter.first * 4) + 255)));
+		punishTextP1->draw();
+	}
+
+	if (displayCounter.second < 1000)
+	{
+		int xPosMessageP2 = 640 - 17 - punishTextP2->rect.width;
+		punishTextP2->setPosition({xPosMessageP2, 67});
+		punishTextP2->tint.a = min(255, max(0, (240 - static_cast<int>(displayCounter.second * 4) + 255)));
+		punishTextP2->draw();
+	}
 }
 
 extern "C" __declspec(dllexport) bool CheckVersion(const BYTE hash[16]) {
@@ -99,9 +163,8 @@ extern "C" __declspec(dllexport) bool Initialize(HMODULE hMyModule, HMODULE hPar
 {
 	DWORD old;
 
-	LoadSettings();
-
 	VirtualProtect((PVOID)RDATA_SECTION_OFFSET, RDATA_SECTION_SIZE, PAGE_EXECUTE_WRITECOPY, &old);
+	og_BattleManagerOnRoundstart = SokuLib::TamperDword(&SokuLib::VTable_BattleManager.onRoundStart, BattleOnRoundstart);
 	og_BattleManagerOnProcess = SokuLib::TamperDword(&SokuLib::VTable_BattleManager.onProcess, BattleOnProcess);
 	og_BattleManagerOnRender  = SokuLib::TamperDword(&SokuLib::VTable_BattleManager.onRender,  BattleOnRender);
 	VirtualProtect((PVOID)RDATA_SECTION_OFFSET, RDATA_SECTION_SIZE, old, &old);
