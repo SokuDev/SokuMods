7fbcf5fba9820f4672d6ed1ea7b10ec4329cb205
Gegel85 <Gegel85@laposte.net>
2021-12-24 18:09:43 +0100

    Added Yukari animation for outro

--- modules/TrialMode/Animations/SakuyaBasicCombos/SakuyaOut6.cpp
+++ modules/TrialMode/Animations/SakuyaBasicCombos/SakuyaOut6.cpp
@@ -7,7 +7,7 @@
 #include "../BattleAnimation.hpp"
 
 #define TRANSLATE_MAX 120
-#define YUKARI_START_LOCATION -500
+#define YUKARI_START_LOCATION 500
 #define CAM_START_LOCATION (-20)
 #define BG_START_LOCATION 700
 #define HIT_STOP 7
@@ -38,17 +38,14 @@ static const std::vector<std::string> dialogs{
 	"   Here you are!"
 };
 
-class Intro : public BattleAnimation {
+class Outro : public BattleAnimation {
 private:
+	SokuLib::DrawUtils::RectangleShape _flashRect;
 	SokuLib::DrawUtils::Sprite _stageBg;
 	SokuLib::DrawUtils::Sprite _stageBottom;
-	bool _falling = true;
-	bool _remiIn = false;
-	unsigned _fightCtr = 120;
-	unsigned _fightStage = 0;
-	unsigned _ctr = 240;
-	unsigned _ctr2;
-	unsigned _ctr3 = 0;
+	bool _pressed = false;
+	bool _side = false;
+	unsigned _ctr = 60;
 	unsigned _currentStage = 0;
 	std::unique_ptr<SokuStand> _dialog;
 	bool _keyPressed = false;
@@ -57,82 +54,130 @@ private:
 	int buffer[0xA];
 	SokuLib::CharacterManager *_yukari = nullptr;
 
+	void _init()
+	{
+		auto &battleMgr = SokuLib::getBattleMgr();
+
+		this->_playerInfo.character = SokuLib::CHARACTER_YUKARI;
+		this->_playerInfo.isRight = false;
+		this->_playerInfo.palette = 0;
+		this->_playerInfo.padding2 = 0;
+		this->_playerInfo.deck = 0;
+		this->_playerInfo.effectiveDeck.unknown1 = 0;
+		this->_playerInfo.effectiveDeck.data = nullptr;
+		this->_playerInfo.effectiveDeck.chunkSize = 0;
+		this->_playerInfo.effectiveDeck.counter = 0;
+		this->_playerInfo.effectiveDeck.size = 0;
+		this->_playerInfo.keyManager = nullptr;
+
+		((void (__thiscall *)(int *, bool, SokuLib::PlayerInfo &))0x46da40)(this->buffer, false, this->_playerInfo);
+		(*(void (__thiscall **)(SokuLib::CharacterManager *))(*(int *)this->_yukari + 0x44))(this->_yukari);
+		*(SokuLib::CharacterManager **)&this->_yukari->objectBase.offset_0x168[8] = &battleMgr.rightCharacterManager;
+
+		SokuLib::camera.scale = 1;
+		//SokuLib::camera.translate.x = CAM_START_LOCATION;
+		//SokuLib::camera.translate.y = 0;
+		//SokuLib::camera.backgroundTranslate.x = BG_START_LOCATION;
+
+		battleMgr.leftCharacterManager.objectBase.position.x = -500;
+		battleMgr.leftCharacterManager.objectBase.action = SokuLib::ACTION_DEFAULT_SKILL1_B;
+
+		battleMgr.rightCharacterManager.objectBase.position.y = 0;
+		battleMgr.rightCharacterManager.objectBase.action = SokuLib::ACTION_IDLE;
+		battleMgr.rightCharacterManager.objectBase.animate();
+
+		this->_yukari->objectBase.position.x = 600;
+		this->_yukari->objectBase.position.y = 800;
+		this->_yukari->objectBase.action = SokuLib::ACTION_ALT2_SKILL4_C;
+		this->_yukari->objectBase.animate();
+		//this->_yukari->objectBase.animate2();
+		this->_yukari->objectBase.renderInfos.color = SokuLib::Color::Black;
+	}
 
 	void stage0()
 	{
 		auto &battleMgr = SokuLib::getBattleMgr();
+		auto color = this->_flashRect.getFillColor();
 
-		if (this->_ctr == 240) {
-			this->_playerInfo.character = SokuLib::CHARACTER_YUKARI;
-			this->_playerInfo.isRight = false;
-			this->_playerInfo.palette = 0;
-			this->_playerInfo.padding2 = 0;
-			this->_playerInfo.deck = 0;
-			this->_playerInfo.effectiveDeck.unknown1 = 0;
-			this->_playerInfo.effectiveDeck.data = nullptr;
-			this->_playerInfo.effectiveDeck.chunkSize = 0;
-			this->_playerInfo.effectiveDeck.counter = 0;
-			this->_playerInfo.effectiveDeck.size = 0;
-			this->_playerInfo.keyManager = nullptr;
+		battleMgr.leftCharacterManager.objectBase.doAnimation();
+		battleMgr.rightCharacterManager.objectBase.doAnimation();
+		if (!color.a && !this->_ctr && this->_side) {
+			// Yukari arrives init
+			this->_yukari->objectBase.position.y = 250;
+			this->_yukari->playSE(1);
+			spawnSubObject(*this->_yukari, 0x33B, 200, 500);
 
-			((void (__thiscall *)(int *, bool, SokuLib::PlayerInfo &))0x46da40)(this->buffer, false, this->_playerInfo);
-			(*(void (__thiscall **)(SokuLib::CharacterManager *))(*(int *)this->_yukari + 0x44))(this->_yukari);
-			*(SokuLib::CharacterManager **)&this->_yukari->objectBase.offset_0x168[8] = &battleMgr.rightCharacterManager;
+			auto obj = this->_yukari->objects.list.vector().back();
 
-			SokuLib::camera.scale = 0.8;
-			SokuLib::camera.translate.x = CAM_START_LOCATION;
-			SokuLib::camera.translate.y = 0;
-			SokuLib::camera.backgroundTranslate.x = BG_START_LOCATION;
-
-			battleMgr.leftCharacterManager.objectBase.position.x = -500;
-			battleMgr.leftCharacterManager.objectBase.action = SokuLib::ACTION_DEFAULT_SKILL1_B;
-
-			battleMgr.rightCharacterManager.objectBase.position.y = 0;
-			battleMgr.rightCharacterManager.objectBase.action = SokuLib::ACTION_IDLE;
-			battleMgr.rightCharacterManager.objectBase.animate();
-
-			this->_yukari->objectBase.position.x = YUKARI_START_LOCATION;
-			this->_yukari->objectBase.position.y = 0;
-			this->_yukari->objectBase.action = SokuLib::ACTION_WALK_FORWARD;
-			this->_yukari->objectBase.animate();
-
-			((void (*)(const char *))0x43ff10)("data/bgm/st20.ogg");
-		}
-
+			while (obj->actionBlockId != 3)
+				obj->animate2();
+			this->_yukari->objectBase.doAnimation();
+			updateSubObjects(*this->_yukari);
+			this->_yukari->objectBase.position.y = 800;
+			this->_yukari->objectBase.animate2();
 			this->_currentStage++;
+		}
+		if (!this->_side) {
+			color.a += 0x11;
+			if (color.a == 0xFF) {
+				this->_side = true;
+				this->_init();
+				while (battleMgr.rightCharacterManager.objectBase.actionBlockId != 3 && battleMgr.rightCharacterManager.objectBase.animationCounter != 2)
+					battleMgr.rightCharacterManager.objectBase.doAnimation();
+			}
+		} else if (this->_flashRect.getFillColor().a)
+			color.a -= 0x11;
+		else if (this->_ctr)
+			this->_ctr--;
+		this->_flashRect.setFillColor(color);
 	}
 
 	void stage1()
 	{
-//		auto &battleMgr = SokuLib::getBattleMgr();
-
-//		battleMgr.leftCharacterManager.objectBase.doAnimation();
-//		battleMgr.rightCharacterManager.objectBase.doAnimation();
-//		updateSubObjects(*_yukari);
-		this->_currentStage++;
-//		spawnSubObject(*_yukari, 0x41, 500, 0);
+		updateSubObjects(*this->_yukari);
+		if (this->_yukari->objects.list.size && this->_yukari->objects.list.head->next->val->animationCounter == 14) {
+			this->_yukari->playSE(4);
+			this->_yukari->playSE(5);
+			this->_yukari->objectBase.position.y = 0;
+			this->_currentStage++;
+		}
 	}
 
 	void stage2()
 	{
-		auto &battleMgr = SokuLib::getBattleMgr();
-
-		battleMgr.leftCharacterManager.objectBase.doAnimation();
-		battleMgr.rightCharacterManager.objectBase.doAnimation();
+		this->_ctr ^= 1;
+		if (this->_ctr)
+			this->_yukari->objectBase.doAnimation();
+		else if (this->_yukari->objectBase.actionBlockId == 0) {
+			this->_yukari->objectBase.action = SokuLib::ACTION_IDLE;
+			this->_yukari->objectBase.animate();
+			this->_currentStage++;
+		}
 	}
 
-	std::vector<void (Intro::*)()> anims{
-		&Intro::stage0,
-		&Intro::stage1,
-		&Intro::stage2
+	void stage3()
+	{
+		updateSubObjects(*this->_yukari);
+		this->_yukari->objectBase.doAnimation();
+	}
+
+	std::vector<void (Outro::*)()> anims{
+		&Outro::stage0,
+		&Outro::stage1,
+		&Outro::stage2,
+		&Outro::stage3
 	};
 
 public:
-	Intro()
+	Outro()
 	{
 		puts("Init intro.");
 
-		this->_stageBg.texture.loadFromFile((profilePath + std::string("\\bnbcorn1intro.png")).c_str());
+		this->_flashRect.setFillColor(SokuLib::DrawUtils::DxSokuColor{0, 0, 0, 0x0});
+		this->_flashRect.setBorderColor(SokuLib::DrawUtils::DxSokuColor{0, 0, 0, 0});
+		this->_flashRect.setSize({640, 480});
+
+		this->_stageBg.texture.loadFromFile((profilePath + std::string("\\Lore6Outro.png")).c_str());
 		this->_stageBg.setSize(this->_stageBg.texture.getSize());
 		this->_stageBg.setPosition({
 			static_cast<int>(320 - this->_stageBg.texture.getSize().x / 2),
@@ -155,7 +200,7 @@ public:
 		this->_dialog = std::make_unique<SokuStand>(dialogs);
 	}
 
-	~Intro()
+	~Outro()
 	{
 		if (this->_yukari) {
 			(*(void (__thiscall **)(SokuLib::CharacterManager *, char))this->_yukari->objectBase.vtable)(this->_yukari, 0);
@@ -181,13 +226,17 @@ public:
 
 	void render() const override
 	{
-		if (this->_currentStage <= 1 && this->_yukari) {
+		this->_flashRect.draw();
+		if (this->_yukari) {
+			// Display the CharacterManager subobjects
+			((void (__thiscall *)(SokuLib::ObjListManager &, int))0x59be00)(this->_yukari->objects, -2);
+			((void (__thiscall *)(SokuLib::ObjListManager &, int))0x59be00)(this->_yukari->objects, -1);
+
 			// Display the CharacterManager
 			((void (__thiscall *)(SokuLib::CharacterManager &))0x438d20)(*this->_yukari);
 
-			// Display the CharacterManager subobjects
-			((void (__thiscall *)(SokuLib::ObjListManager &, int))0x59be00)(this->_yukari->objects, -1);
 			((void (__thiscall *)(SokuLib::ObjListManager &, int))0x59be00)(this->_yukari->objects, 1);
+			((void (__thiscall *)(SokuLib::ObjListManager &, int))0x59be00)(this->_yukari->objects, 2);
 
 			// We redraw Remilia because Reisen's subobjects mess up the DirectX context and this will clean it up
 			((void (__thiscall *)(SokuLib::CharacterManager &))0x438d20)(SokuLib::getBattleMgr().leftCharacterManager);
@@ -196,7 +245,7 @@ public:
 			this->_stageBg.draw();
 			this->_stageBottom.draw();
 		}
-//		this->_dialog->render();
+		this->_dialog->render();
 	}
 
 	void onKeyPressed() override
@@ -209,7 +258,7 @@ extern "C"
 {
 	__declspec(dllexport) BattleAnimation *getOutro()
 	{
-		return new Intro();
+		return new Outro();
 	}
 
 	int APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID lpReserved)
