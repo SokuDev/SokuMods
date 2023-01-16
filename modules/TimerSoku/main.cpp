ffab1887208157ec56b6e109bc0418643543f615
PinkySmile <Gegel85@laposte.net>
2023-01-16 19:37:10 +0100

    Fixed BGM and music issues

--- modules/TimerSoku/main.cpp
+++ modules/TimerSoku/main.cpp
@@ -2,6 +2,7 @@
 // Created by Gegel85 on 31/10/2020
 //
 
+#include <cmath>
 #include <SokuLib.hpp>
 #include <shlwapi.h>
 
@@ -10,7 +11,7 @@
 
 const unsigned multiplier = std::ceil(EXPECTED_DURATION * 60 / 99);
 
-static int (SokuLib::BattleManager::*ogBattleMgrOnProcess)();
+static void (__fastcall *ogBattleMgrUpdateCounters)(void *);
 static void (SokuLib::BattleManager::*ogBattleMgrOnRender)();
 static SokuLib::DrawUtils::Sprite digits;
 static SokuLib::SWRFont font;
@@ -43,18 +44,45 @@ int __fastcall CBattleManager_OnRender(SokuLib::BattleManager *This)
 	return 0;
 }
 
+static bool musicPlayed = false;
+bool *magicLocation = nullptr;
+
 void punish(SokuLib::CharacterManager &chr, unsigned short &timer, unsigned char &orb)
 {
 	if (orb == 0) {
+		if (timer & 0x4000)
+			return;
 		timer |= 0x4000;
+		puts("Activate Typhoon");
 		return SokuLib::activateWeather(SokuLib::WEATHER_TYPHOON, 1);
 	}
-	SokuLib::playSEWaveBuffer(38);
 	orb--;
+	puts("Crush");
+	SokuLib::playSEWaveBuffer(38);
 }
 
-int __fastcall CBattleManager_OnProcess(SokuLib::BattleManager *This)
+static void playBGM(const char *bgm, bool expectedState)
 {
+	if (magicLocation) {
+		if (musicPlayed == expectedState)
+			return (void)puts("musicPlayed == expectedState");
+		if (*(unsigned *)0x8985D8 != *(unsigned *)(magicLocation + 0x20))
+			return (void)printf("PlayBGM %u != %u\n", *(unsigned *)0x8985D8, *(unsigned *)(magicLocation + 0x20));
+		printf("Playing %s\n", bgm);
+		SokuLib::playBGM(bgm);
+		puts("Done");
+		musicPlayed = expectedState;
+		return;
+	}
+	printf("Playing %s\n", bgm);
+	SokuLib::playBGM(bgm);
+	puts("Done");
+}
+
+void __fastcall CBattleManager_OnProcess(void *obj)
+{
+	SokuLib::BattleManager *This = &SokuLib::getBattleMgr();
+
 	if (!init) {
 		digits.texture.loadFromGame("data/battle/weatherFont001.cv0");
 		digits.setSize({22, 36});
@@ -63,23 +91,32 @@ int __fastcall CBattleManager_OnProcess(SokuLib::BattleManager *This)
 		init = true;
 	}
 
-	int ret = (This->*ogBattleMgrOnProcess)();
+	ogBattleMgrUpdateCounters(obj);
 
-	if (This->matchState < 1)
-		return ret;
+	// We detect SokuRoll's hook and we extract an address from it so we can do the same checks
+	if (*(unsigned char *)0x490B29 == 0xE9) {
+		auto hookAddress = *(unsigned *)0x490B2A + 0x490B29 + 5;
+		auto finalHookAddress = *(unsigned *)(hookAddress + 1) + hookAddress + 5;
+
+		magicLocation = *(bool **)(finalHookAddress + 8);
+	} else
+		magicLocation = nullptr;
+	// Second call is to check if the game is paused
+	if (This->matchState < 1 || ((bool (*)())0x0043e740)())
+		return;
 
 	auto &timer = *(unsigned short *)&This->leftCharacterManager.objectBase.offset_0x14E;
 	auto *orbs = (unsigned char *)&This->rightCharacterManager.objectBase.offset_0x14E;
 
+	if (This->matchState == 1)
+		musicPlayed = false;
 	if (This->matchState != 2) {
 		if (timer & 0x4000)
 			SokuLib::weatherCounter = 0;
-		if (timer & 0x8000) {
-			//auto file = "data/bgm/st" + std::string(SokuLib::gameParams.musicId < 10 ? "0" : "") + std::to_string(SokuLib::gameParams.musicId) + ".ogg";
+		if ((timer & 0x8000) && This->currentRound != 0) {
+			auto file = "data/bgm/st" + std::string(SokuLib::gameParams.musicId < 10 ? "0" : "") + std::to_string(SokuLib::gameParams.musicId) + ".ogg";
 
-			//printf("Playing %s\n", file.c_str());
-			//SokuLib::playBGM(file.c_str());
-			//puts("Done");
+			playBGM(file.c_str(), false);
 		}
 		timer = 99 * multiplier;
 		orbs[0] = 5;
@@ -87,11 +124,8 @@ int __fastcall CBattleManager_OnProcess(SokuLib::BattleManager *This)
 	} else if ((timer & 0x4000) == 0) {
 		timer--;
 		if ((timer & 0x3FFF) == 0) {
-			if (!(timer & 0x8000)) {
-				//puts("Playing data/bgm/st36.ogg");
-				//SokuLib::playBGM("data/bgm/st36.ogg");
-				//puts("Done");
-			}
+			if (!(timer & 0x8000))
+				playBGM("data/bgm/st36.ogg", true);
 			timer |= 0x8000;
 			timer |= CRUSH_INTERVAL * 60;
 			if (This->leftCharacterManager.objectBase.hp < This->rightCharacterManager.objectBase.hp)
@@ -107,8 +141,10 @@ int __fastcall CBattleManager_OnProcess(SokuLib::BattleManager *This)
 	if (This->matchState <= 2) {
 		if (!(timer & 0x4000)) {
 			if (timer & 0x8000) {
-				if ((timer & 0x3FFF) % 60 == 0 && (timer & 0x3FFF) != CRUSH_INTERVAL * 60)
+				if ((timer & 0x3FFF) % 60 == 0 && (timer & 0x3FFF) != CRUSH_INTERVAL * 60) {
+					puts("Tick");
 					SokuLib::playSEWaveBuffer(59);
+				}
 				displayed = ((timer & 0x3FFF) + 59) / 60;
 			} else if (timer % multiplier == 0)
 				displayed = (timer + multiplier - 1) / multiplier;
@@ -127,7 +163,6 @@ int __fastcall CBattleManager_OnProcess(SokuLib::BattleManager *This)
 		}
 		displayed |= timer & 0xC000;
 	}
-	return ret;
 }
 
 extern "C" __declspec(dllexport) bool CheckVersion(const BYTE hash[16]) {
@@ -148,7 +183,6 @@ extern "C" __declspec(dllexport) bool Initialize(HMODULE hMyModule, HMODULE hPar
 	// DWORD old;
 	::VirtualProtect((PVOID)RDATA_SECTION_OFFSET, RDATA_SECTION_SIZE, PAGE_EXECUTE_WRITECOPY, &old);
 	ogBattleMgrOnRender  = SokuLib::TamperDword(&SokuLib::VTable_BattleManager.onRender,  CBattleManager_OnRender);
-	ogBattleMgrOnProcess = SokuLib::TamperDword(&SokuLib::VTable_BattleManager.onProcess, CBattleManager_OnProcess);
 	// s_origCLogo_OnProcess   = TamperDword(vtbl_CLogo   + 4, (DWORD)CLogo_OnProcess);
 	// s_origCBattle_OnProcess = TamperDword(vtbl_CBattle + 4, (DWORD)CBattle_OnProcess);
 	// s_origCBattleSV_OnProcess = TamperDword(vtbl_CBattleSV + 4, (DWORD)CBattleSV_OnProcess);
@@ -157,6 +191,7 @@ extern "C" __declspec(dllexport) bool Initialize(HMODULE hMyModule, HMODULE hPar
 	// s_origCSelect_OnProcess = TamperDword(vtbl_CSelect + 4, (DWORD)CSelect_OnProcess);
 	::VirtualProtect((PVOID)RDATA_SECTION_OFFSET, RDATA_SECTION_SIZE, old, &old);
 	::VirtualProtect((PVOID)TEXT_SECTION_OFFSET, TEXT_SECTION_SIZE, PAGE_EXECUTE_WRITECOPY, &old);
+	ogBattleMgrUpdateCounters = SokuLib::TamperNearJmpOpr(0x004796b6, CBattleManager_OnProcess);
 	::VirtualProtect((PVOID)TEXT_SECTION_OFFSET, TEXT_SECTION_SIZE, old, &old);
 
 	::FlushInstructionCache(GetCurrentProcess(), nullptr, 0);
